b8db3f8a06747467178dc1fbdca1794997c49ad8
Purva Kulkarni <purvakulkarni7@gmail.com>
2019-03-17 21:26:57 +0100

    PatientID issue and rowname issue resolved

--- Bar_plot/PerturbedFeatureBarPlots.R
+++ Bar_plot/PerturbedFeatureBarPlots.R
@@ -1,4 +1,4 @@
-          ##################################################
+##################################################
 ## Title: PerturbedFeatureBarPlots.R
 ## Description: This function generates patient vs control bar plots where a selected patient of interest is highlighted in red and the other patients in grey.
 ## Input:
@@ -33,11 +33,11 @@ PerturbedFeatureBarPlots <- function()
   # Enter the file path (for example: Sample_data\190125_Patient_plasma_BB18-01585_2019-02-05_ESIpos.tsv)
   patientFilePath <-
     readline(prompt = "Enter interested patient file path: ")
-
+  
   # Enter the file path (for example: Sample_data\190125_ControlsvsPatients_2019-02-05_ESIpos.tsv)
   CoVsPaFilePath <-
     readline(prompt = "Enter control vs Patient file path: ")
-
+  
   # Feature filter options
   cat("Enter the different filter options below:")
   mp <-
@@ -47,7 +47,7 @@ PerturbedFeatureBarPlots <- function()
     mp <- "YES"
     cat(mp)
   }
-
+  
   mass_delta <-
     as.numeric(readline(prompt = "Enter value to filter mass (ppm) [Press ENTER to use default = 5]: "))
   if (is.na(mass_delta))
@@ -55,7 +55,7 @@ PerturbedFeatureBarPlots <- function()
     mass_delta <- as.numeric(5)
     cat(mass_delta)
   }
-
+  
   RT_delta_min <-
     as.numeric(readline(prompt = "Enter minimum value to filter RT [press ENTER to use default = -1.0]: "))
   if (is.na(RT_delta_min))
@@ -63,7 +63,7 @@ PerturbedFeatureBarPlots <- function()
     RT_delta_min <- as.numeric(-1)
     cat(RT_delta_min)
   }
-
+  
   RT_delta_max <-
     as.numeric(readline(prompt = "Enter maximum value to filter RT [press ENTER to use default = 10]: "))
   if (is.na(RT_delta_max))
@@ -71,7 +71,7 @@ PerturbedFeatureBarPlots <- function()
     RT_delta_max <- as.numeric(10)
     cat(RT_delta_max)
   }
-
+  
   BFH_Pval <-
     as.numeric(
       readline(prompt = "Enter value to filter Bonferroni Holm P values [press ENTER to use default = 0.05]: ")
@@ -79,9 +79,9 @@ PerturbedFeatureBarPlots <- function()
   if (is.na(BFH_Pval))
   {
     BFH_Pval <- as.numeric(0.05)
-    cat(paste(BFH_Pval,"\n\n"))
+    cat(paste(BFH_Pval, "\n\n"))
   }
-
+  
   # Read patient data file
   patientFile <-
     read.table(
@@ -92,27 +92,23 @@ PerturbedFeatureBarPlots <- function()
       quote = "",
       stringsAsFactors = FALSE
     )
-
-  rawDataIdentifier = patientFile[which(patientFile$Feature_ID == "Raw Data Identifiers"),2]
+  
+  # Extract the patient identifier from the patient file 
+  rawDataIdentifier = patientFile[which(patientFile$Feature_ID == "Raw Data Identifiers"), 2]
   tempString = str_split(rawDataIdentifier, ";")
   rawDataIdentifier = tempString[[1]][1]
-
+  
   data_table <- read.table(CoVsPaFilePath, fill = TRUE)
-  patientID <- as.character(data_table[2,((which(data_table[1,1:ncol(data_table)] == rawDataIdentifier)) + 3)])
-  data_table <- data_table[(2:nrow(data_table)),]
-  colnames(data_table) <- as.character(unlist(data_table[1,]))
-  data_table <- data_table[(2:nrow(data_table)),]
-
+  patientID <-
+    as.character(data_table[2, ((which(data_table[1, 1:ncol(data_table)] == rawDataIdentifier)) + 3)])
+  
+  data_table <- data_table[(2:nrow(data_table)), ]
+  colnames(data_table) <- as.character(unlist(data_table[1, ]))
+  data_table <- data_table[(2:nrow(data_table)), ]
+  
   patientFile = patientFile[1:(which(patientFile$Feature_ID == "Worklist") -
-                                 1), ]
-
-  # # Generate patientID using patient file name
-  # patientFileName <- basename(patientFilePath)
-  # temp <- str_split(patientFileName, "[_,-]+")
-  # patientID <-
-  #   paste(temp[[1]][2], temp[[1]][3], temp[[1]][4], sep = "_")
-  # patientID <- paste(patientID, temp[[1]][5], sep = ".")
-
+                                 1),]
+  
   # Extract acquisition mode information
   patientFileName <- basename(patientFilePath)
   temp <- str_split(patientFileName, "[_,-]+")
@@ -120,12 +116,12 @@ PerturbedFeatureBarPlots <- function()
     mode = "ESIpos"
   else if (temp[[1]][length(temp[[1]])] == "ESIneg.tsv")
     mode = "ESIneg"
-
+  
   y = 1
-
+  
   # Define a empty list of perturbed feature IDs
   perturbedFeatureID = list()
-
+  
   # Perform filtering in the patient data based on specified filtering parameters
   for (i in 1:nrow(patientFile))
   {
@@ -141,10 +137,10 @@ PerturbedFeatureBarPlots <- function()
       y = y + 1
     }
   }
-
+  
   # Remove duplicates in the perturbed feature ID list
   perturbedFeatureID <- unique(perturbedFeatureID)
-
+  
   # Print the number of perturbed features found after filtering
   cat(
     paste(
@@ -152,46 +148,43 @@ PerturbedFeatureBarPlots <- function()
       "statistically significant features found based on filtering. Now printing bar plots..."
     )
   )
-
-
-  temp <- colnames(data_table)
-
+  
+  # Handle duplicate rownames 
+  tempList <- colnames(data_table)
+  
   j = 1
   k = 1
-
-  for(i in 5:length(temp))
+  
+  for (i in 5:length(tempList))
   {
-
-    if(startsWith(temp[i], "C") || startsWith(temp[i], "P"))
+    if (startsWith(tempList[i], "C") || startsWith(tempList[i], "P"))
     {
-      if((i %% 2) != 0)
-        temp[i] = paste(temp[i],".1", sep = "")
+      if ((i %% 2) != 0)
+        tempList[i] = paste(tempList[i], ".1", sep = "")
     }
-    else if(startsWith(temp[i], "Q"))
+    else if (startsWith(tempList[i], "Q"))
     {
-
-      temp[i] = paste(temp[i],".", j, sep = "")
-      j = j +1
+      tempList[i] = paste(tempList[i], ".", j, sep = "")
+      j = j + 1
     }
-    else if(startsWith(temp[i], "V"))
+    else if (startsWith(tempList[i], "V"))
     {
-
-      temp[i] = paste(temp[i],".", k, sep = "")
-      k = k +1
+      tempList[i] = paste(tempList[i], ".", k, sep = "")
+      k = k + 1
     }
   }
-
+  
   # Extract requires column information from the patient vs coltrol data
   data_table_t <- as.data.frame(t(data_table))
   FeatureIdColumn <- data_table[1]
   MassColumn <- data_table[2]
   RTColumn <- data_table[3]
-  data_table_t_sub <- data_table_t[4:nrow(data_table_t),]
-  row.names(data_table_t_sub) <- temp[4:length(temp)]
-
+  data_table_t_sub <- data_table_t[4:nrow(data_table_t), ]
+  row.names(data_table_t_sub) <- tempList[4:length(tempList)]
+  
   patientIDColumnNumber = which(rownames(data_table_t_sub) == patientID)
   SampleType <- row.names(data_table_t_sub)
-
+  
   # Find the Sample type based on column names in patient vs coltrol data
   for (i in 1:length(SampleType))
   {
@@ -202,17 +195,17 @@ PerturbedFeatureBarPlots <- function()
     else
       SampleType[i] <- word(SampleType[i], sep = "_")
   }
-
+  
   data_table_t_sub <- cbind(data_table_t_sub, SampleType)
-
+  
   #log.data <- log(data_table_sub_t_sub_ST[,1:10000])
   data <- data_table_t_sub[, (1:(ncol(data_table_t_sub) - 1))]
-
+  
   # The following step is to resolve the error
   # cannot rescale a constant/zero column to unit variance (Ref: https://stackoverflow.com/questions/40315227/how-to-solve-prcomp-default-cannot-rescale-a-constant-zero-column-to-unit-var)
   data <- data[, apply(data, 2, var) != 0]
   SampleType <- data_table_t_sub[, ncol(data_table_t_sub)]
-
+  
   # pal <- c(
   #   "Controle" = "#000000",
   #   "Patient" = "#9e9e9e",
@@ -220,39 +213,41 @@ PerturbedFeatureBarPlots <- function()
   #   "QC" = "#0000ff",
   #   "Validation" = "#008700"
   # )
-
+  
   # Generate a color pallete to color fill bars from different sample type
   pal <-
     setNames(c("#000000", "#9e9e9e", "#FF0000", "#0000ff", "#008700"),
              levels(SampleType))
-
+  
   # set row and column names for the transposed data frame
   columnNames <- colnames(data_table)
   columnNames <- columnNames[4:length(columnNames)]
   rownames(data) = make.names(columnNames, unique = TRUE)
   colnames(data) = FeatureIdColumn$F
-
+  
   # Generate a new directory to export the bar plots
   newDir = paste(patientID, "OutputPlots", mode, sep = "_")
   makeDir(newDir)
   outputPath = paste(".", "/", newDir, sep = "")
-
+  
   # Create an empty plotlist to save ggplot objects
   plotList = list()
-
+  
   # For loop to generate a barplot for each pertubed feature
   for (x in 1:length(perturbedFeatureID))
   {
     # Extract single feature ID and related information
     featureID <- as.numeric(perturbedFeatureID[[x]])
     temp = data[as.character(featureID)]
-     df <-
-      data.frame(Sample = c(1:nrow(data)), Intensity = c(temp[, 1]))
+    
+    df <-
+      data.frame(Sample = c(1:nrow(data)), Intensity = c(as.numeric(as.character(temp[,1]))))
+    
     rownames(df) <- rownames(data)
-
+    
     # Generate plot name
     plotName <- paste("Plot", featureID, sep = "_")
-
+    
     # Create the ggplot (themes used here are compatible with combined bar plots displayed in a grid)
     p <-
       ggplot(data = df, aes(x = Sample, y = Intensity, fill = SampleType)) +
@@ -283,10 +278,10 @@ PerturbedFeatureBarPlots <- function()
       theme(legend.position = "none") +
       theme(axis.text = element_text(size = 4),
             axis.title = element_text(size = 4, face = "bold"))
-
+    
     # Save the ggPlot object in the plotList
     plotList[[x]] = p
-
+    
     # Add additional themes for exporting individual bar plots
     suppressMessages(
       p <- p + theme(
@@ -307,11 +302,11 @@ PerturbedFeatureBarPlots <- function()
           axis.title = element_text(size = 7, face = "bold")
         )
     )
-
+    
     # Generate name for bar plot image file
     plotFileName <-
       paste(featureID, patientID, mode, ".png", sep = "_")
-
+    
     # Save the bar plot
     suppressMessages(
       ggsave(
@@ -325,15 +320,15 @@ PerturbedFeatureBarPlots <- function()
       )
     )
   }
-
+  
   # Generate combined bar plot and save in a grid manner (16 plots per sheet)
   if (length(plotList) > 16)
   {
     plotNumber <- length(plotList)
     FullPlotSheetNumber = plotNumber %/% 16
-
+    
     RemainderPlotSheet = plotNumber %% 16
-
+    
     for (i in 1:FullPlotSheetNumber)
     {
       temp <- n2mfrow(16)
@@ -349,7 +344,7 @@ PerturbedFeatureBarPlots <- function()
         height = 4.8
       )
     }
-
+    
     if (RemainderPlotSheet > 0)
     {
       temp <- n2mfrow(RemainderPlotSheet)
@@ -363,7 +358,7 @@ PerturbedFeatureBarPlots <- function()
         height = 4.8
       )
     }
-
+    
   }
   else
   {
@@ -377,7 +372,7 @@ PerturbedFeatureBarPlots <- function()
       height = 4.8
     )
   }
-
+  
   cat("\nAll barplots successfully generated!\n")
   cat("==================================================\n")
   # End of function
